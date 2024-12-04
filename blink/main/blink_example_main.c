057dd31e127de847b76222c546c34bb9831c8a37
bavendanom <bavendanom@unal.edu.co>
2024-12-04 00:35:19 -0500

    Ejemplo del blink tomado desde los ejemplos ESP-IDDF

--- blink/main/blink_example_main.c
+++ blink/main/blink_example_main.c
@@ -98,7 +98,7 @@ void app_main(void)
         ESP_LOGI(TAG, "Turning the LED %s!", s_led_state == true ? "ON" : "OFF");
         blink_led();
         /* Toggle the LED state */
-        s_led_state = !s_led_state;
+        s_led_state = !s_led_state; // cambiar estado
         vTaskDelay(CONFIG_BLINK_PERIOD / portTICK_PERIOD_MS);
     }
 }
