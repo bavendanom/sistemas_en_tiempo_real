f7cdac65bcbc38c49b651e127f994e98aaa477d8
bavendanom <bavendanom@unal.edu.co>
2024-12-26 21:12:00 -0500

    version:2 -- se realizaron algunos cambios en las librerias, cosas que son generales se omiten para mejorar la estructure del main

--- led_RGB/main/led_RGB_LIBRARY/src/led_RGB_library.c
+++ led_RGB/main/led_RGB_LIBRARY/src/led_RGB_library.c
@@ -4,16 +4,17 @@
 #include "freertos/FreeRTOS.h"
 #include <math.h>
 #include "led_RGB_library.h"
+
 #define LEDC_DUTY_RES           LEDC_TIMER_13_BIT // Set duty resolution to 13 bits
 #define LEDC_FREQUENCY          (4000) // Frequency in Hertz. Set frequency at 4 kHz
 
 
 // Prepare and then apply the LEDC PWM timer configuration
-esp_err_t ledc_init_timer(led l) {
+esp_err_t ledc_init_timer(void) {
     ledc_timer_config_t ledc_timer = {
-        .speed_mode       = l.speed_mode,
+        .speed_mode       = LEDC_LOW_SPEED_MODE,// Modo de velocidad (LEDC_HIGH_SPEED_MODE o LEDC_LOW_SPEED_MODE)
         .duty_resolution  = LEDC_DUTY_RES,
-        .timer_num        = l.timer_sel,
+        .timer_num        = LEDC_TIMER_0,
         .freq_hz          = LEDC_FREQUENCY,  // Set output frequency at 4 kHz
         .clk_cfg          = LEDC_AUTO_CLK
     };
@@ -23,26 +24,39 @@ esp_err_t ledc_init_timer(led l) {
 esp_err_t ledc_init_channel(led l) {
     ledc_channel_config_t ledc_channel = {  
         .channel        = l.channel,
-        .speed_mode     = l.speed_mode,
-        .timer_sel      = l.timer_sel,
-        .intr_type      = l.intr_type,
+        .speed_mode     = LEDC_LOW_SPEED_MODE,
+        .timer_sel      = LEDC_TIMER_0,
+        .intr_type      = LEDC_INTR_DISABLE,
         .gpio_num       = l.gpio_num,
         .duty           = l.duty,
         .hpoint         = 0,
-        .flags.output_invert = 1
+        .flags.output_invert = 1 /*!< Enable (1) or disable (0) gpio output invert */
     };
     return ledc_channel_config(&ledc_channel);
 }
+
+
+esp_err_t ledc_initialize_rgb(led_RGB rgb){
+    led leds[] = {rgb.led_red, rgb.led_green, rgb.led_blue};
+    for (int i = 0; i < 3; i++) {
+        ledc_init_channel(leds[i]);
+    }
+    return ESP_OK;
+}
+
 
 static uint32_t set_duty_porcent(uint32_t duty_porcent) {
     return ((1 << LEDC_TIMER_13_BIT) * duty_porcent) / 100;
 }
 
 static esp_err_t set_led_color(led l, uint32_t duty) {
+    if (duty > 100) {
+        return ESP_ERR_INVALID_ARG; // Retorna error si el duty es inválido
+    }
     // Calcular y configurar el duty
     uint32_t scaled_duty = set_duty_porcent(duty);
-    ledc_set_duty(l.speed_mode, l.channel, scaled_duty);
-    return ledc_update_duty(l.speed_mode, l.channel);
+    ledc_set_duty(LEDC_LOW_SPEED_MODE, l.channel, scaled_duty);
+    return ledc_update_duty(LEDC_LOW_SPEED_MODE, l.channel);
 }
 
 esp_err_t rgb_set_color(led_RGB rgb, uint32_t duty_red, uint32_t duty_green, uint32_t duty_blue) {
--- led_RGB/main/ledc_basic_example_main.c
+++ led_RGB/main/ledc_basic_example_main.c
@@ -12,69 +12,48 @@
 #include "freertos/FreeRTOS.h"
 #include "led_RGB_LIBRARY/include/led_RGB_library.h"
 
-#define LEDC_OUTPUT_IO_RED        (21)   // GPIO para el LED rojo
-#define LEDC_OUTPUT_IO_GREEN      (18)  // GPIO para el LED verde
+#define LEDC_OUTPUT_IO_RED        (5)   // GPIO para el LED rojo
+#define LEDC_OUTPUT_IO_GREEN      (19)  // GPIO para el LED verde
 #define LEDC_OUTPUT_IO_BLUE       (17)   // GPIO para el LED azul
-#define LEDC_DUTY_INITAL          (0)   // Duty inicial (apagado)Set duty to 50%. (2 ** 13) * 50% = 4096
-
-
-/* Warning:
- * For ESP32, ESP32S2, ESP32S3, ESP32C3, ESP32C2, ESP32C6, ESP32H2, ESP32P4 targets,
- * when LEDC_DUTY_RES selects the maximum duty resolution (i.e. value equal to SOC_LEDC_TIMER_BIT_WIDTH),
- * 100% duty cycle is not reachable (duty cannot be set to (2 ** SOC_LEDC_TIMER_BIT_WIDTH)).
- */
+#define LEDC_DUTY_INITAL          (0)   // Duty inicial (apagado) Set duty to 50%. (2 ** 13) * 50% = 4096
 
 // Estructura para configurar el LED RGB
 led_RGB mi_led_rgb = {
         .led_red = {
             .gpio_num    = LEDC_OUTPUT_IO_RED,
-            .speed_mode  = LEDC_LOW_SPEED_MODE,
             .channel     = LEDC_CHANNEL_0,
-            .intr_type   = LEDC_INTR_DISABLE,
-            .timer_sel   = LEDC_TIMER_0,
             .duty        = LEDC_DUTY_INITAL
         },
         .led_green = {
             .gpio_num    = LEDC_OUTPUT_IO_GREEN,
-            .speed_mode  = LEDC_LOW_SPEED_MODE,
             .channel     = LEDC_CHANNEL_1,
-            .intr_type   = LEDC_INTR_DISABLE,
-            .timer_sel   = LEDC_TIMER_0,
             .duty        = LEDC_DUTY_INITAL
         },
         .led_blue = {
             .gpio_num    = LEDC_OUTPUT_IO_BLUE,
-            .speed_mode  = LEDC_LOW_SPEED_MODE,
             .channel     = LEDC_CHANNEL_2,
-            .intr_type   = LEDC_INTR_DISABLE,
-            .timer_sel   = LEDC_TIMER_0,
             .duty        = LEDC_DUTY_INITAL
         }
     };
 
 // Función para inicializar un LED RGB
 static void rgb_init(led_RGB rgb) {
-    // LEDs individuales (rojo, verde, azul)
-    led leds[] = {rgb.led_red, rgb.led_green, rgb.led_blue};
-
     // Inicializar timers y canales de cada LED
-    for (int i = 0; i < 3; i++) {
-        ledc_init_timer(leds[i]);
-        ledc_init_channel(leds[i]);
-    }
+    ledc_init_timer();
+    ledc_initialize_rgb(rgb);
 }
 
 void app_main(void)
 {
     // Inicializa el RGB
     rgb_init(mi_led_rgb);
-    // Establece un color (por ejemplo, full rojo, sin verde, sin azul)
+    // Establece un color (por ejemplo, sin rojo, sin verde, azul variando)
     while (1)
     {
         int max_value = 100;
         for (int i = 0; i < max_value; i++)
         {
-            rgb_set_color(mi_led_rgb, i,i,i);
+            rgb_set_color(mi_led_rgb, 0,0,i);
             vTaskDelay(100/portTICK_PERIOD_MS);
         }
     }
